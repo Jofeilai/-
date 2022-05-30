794838deca5de8ef2ffd9b600dc42b94cae89561
Jofeilai <97676383+Jofeilai@users.noreply.github.com>
2022-05-31 00:00:43 +0800

    設計查詢訂單介面及從會員連結

--- app/src/main/java/com/example/foodorder/Customer.java
+++ app/src/main/java/com/example/foodorder/Customer.java
@@ -2,13 +2,31 @@ package com.example.foodorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Customer extends AppCompatActivity {
-
+    Button order, searchorder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer2);
+        setContentView(R.layout.activity_customer);
+
+        order = findViewById(R.id.order);
+        searchorder = findViewById(R.id.searchorder);
+
+        order.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Customer.this,Customer2.class));
+            }
+        });
+        searchorder.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Customer.this,Searchorder.class));
+            }
+        });
+
     }
 }
