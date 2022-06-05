cc8cd237ca589633abd654fb762c191be1bf62f7
allen <allenxy5123@yahoo.com.tw>
2022-06-06 01:48:11 +0800

    6/6

--- app/src/main/java/com/example/foodorder/Customer.java
+++ app/src/main/java/com/example/foodorder/Customer.java
@@ -1,30 +1,50 @@
 package com.example.foodorder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Customer extends AppCompatActivity {
-    Button order, searchorder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer);
+        setContentView(R.layout.activity_customer2);
 
-        order = findViewById(R.id.order);
-        searchorder = findViewById(R.id.searchorder);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
 
-        order.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(Customer.this,Customer2.class));
-            }
-        });
-        searchorder.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(Customer.this,Searchorder.class));
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        return;
+
+                    case R.id.map:
+                        startActivity(new Intent(getApplicationContext(), Customer_Map.class));
+                        finish();
+                        overridePendingTransition(0,0);
+                        return;
+
+                    case R.id.search:
+                        startActivity(new Intent(getApplicationContext(), Customer_Search.class));
+                        finish();
+                        overridePendingTransition(0,0);
+                        return;
+
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), Customer_Profile.class));
+                        finish();
+                        overridePendingTransition(0,0);
+                        return;
+                }
             }
         });
 
